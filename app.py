31c013f8b56680a14fb9ba0326b2b01d38991a20
Muhamad Shuro Fadhillah <shuro.fadhillah@gmail.com>
2022-04-14 23:01:33 +0700

    update

--- app.py
+++ app.py
@@ -12,11 +12,11 @@ from flask_cors import CORS
 from flask_sqlalchemy import SQLAlchemy
 from functools import wraps
 from sqlalchemy import Column, Integer, DateTime, func
-
+from datetime import datetime
 from flask_jwt_extended import create_access_token
 from flask_jwt_extended import JWTManager
 import os
-from datetime import datetime
+
 
 app = Flask(__name__)
 jwt = JWTManager(app)
@@ -25,10 +25,11 @@ db = SQLAlchemy(app)
 CORS(app)
 
 filename = os.path.dirname(os.path.abspath(__file__))
-database = 'sqlite:///' + os.path.join(filename, 'uts-WebServices.db')
+database = 'sqlite:///' + os.path.join(filename, 'uts.db')
 app.config['SQLALCHEMY_DATABASE_URI'] = database
 app.config['SECRET_KEY'] = "Poltek"
 
+# Membuat Model
 class users(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(20))
@@ -47,14 +48,13 @@ class event(db.Model):
     event_finish_lng= db.Column(db.String(20)) 
     created_at = db.Column(db.DateTime(timezone=True),default=func.now())
 
-class logs(db.Model):
+class logss(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username= db.Column(db.String(20))
     event_name= db.Column(db.String(20))
     log_lat= db.Column(db.String(20))
     log_lng= db.Column(db.String(20))
     created_at = db.Column(db.DateTime(timezone=True),default=func.now())
-
 
 db.create_all()
 
@@ -64,6 +64,7 @@ class create(Resource):
         dataPassword = request.json.get('password', None)
 
         if dataUsername and dataPassword:
+
             access_token = create_access_token(identity=dataUsername)
             dataModel = users(username=dataUsername, password=dataPassword, token = access_token)
             db.session.add(dataModel)
@@ -71,14 +72,13 @@ class create(Resource):
             return make_response(jsonify({"msg" : "Registrasi Sukses"}), 200)
         return jsonify({"success" : False,"msg":"Username dan Password harus diisi"})
 
-class login(Resource):
+class Login(Resource):
     def post(self):
         dataUsername = request.json.get('username')
         dataPassword = request.json.get('password')
 
         queryUsername = [data.username for data in users.query.all()]
         queryPassword = [data.password for data in users.query.all()]
-
         if dataUsername in queryUsername and dataPassword in queryPassword:
             access_token = create_access_token(identity=dataUsername)
             DB_user = users.query.filter_by(username=dataUsername).first()
@@ -103,7 +103,8 @@ class events(Resource):
         queryToken = [data.token for data in users.query.all()]
         if dataToken in queryToken:
             DB_user = users.query.filter_by(token=dataToken).first()
-        if data_event_name and data_event_start_time and data_event_end_time and data_event_start_lat and data_event_finish_lat and data_event_finish_lng:          
+        if data_event_name and data_event_start_time and data_event_end_time and data_event_start_lat and data_event_finish_lat and data_event_finish_lng:
+            
             dataModel = event(event_creator=DB_user.username,event_name=data_event_name, event_start_time=data_event_start_time,
             event_end_time=data_event_end_time, event_start_lat=data_event_start_lat, event_finish_lat=data_event_finish_lat,
             event_finish_lng=data_event_finish_lng )
@@ -122,7 +123,8 @@ class log(Resource):
         queryToken = [data.token for data in users.query.all()]
         if dataToken in queryToken:
             DB_user = users.query.filter_by(token=dataToken).first()
-        if data_event_name and data_log_lat and data_log_lng :            
+        if data_event_name and data_log_lat and data_log_lng :
+            
             dataModel = logss(username=DB_user.username,event_name=data_event_name,log_lat=data_log_lat,log_lng=data_log_lng )
             db.session.add(dataModel)
             db.session.commit()
@@ -132,16 +134,18 @@ class log(Resource):
 class logs(Resource):
     def get(self):
         data_event_name = request.json.get('event_name')
+
         if data_event_name  :
             query = "SELECT * FROM logss WHERE event_name ='" + data_event_name + "' order by created_at"
-            logs = db.engine.execute(query)                
+            logs = db.engine.execute(query)    
+
+            
             return make_response(jsonify({"hasil":[dict(row) for row in logs]}), 200)
         return jsonify({"success" : False,"msg":"Data tidak dilengkap"})
 
-
-#routes
+# Membuat Routes
 api.add_resource(create, "/api/v1/users/create", methods=["POST"])
-api.add_resource(login, "/api/v1/users/login", methods=["POST"])
+api.add_resource(Login, "/api/v1/users/login", methods=["POST"])
 api.add_resource(events, "/api/v1/events/create", methods=["POST"])
 api.add_resource(log, "/api/v1/events/logs", methods=["POST"])
 api.add_resource(logs, "/api/v1/events/log")
